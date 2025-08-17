3104de9d93ddbd0fbecb5e7babea7d39a8197fff
Samarth Kamble <kamblesamarth04@gmail.com>
2025-08-17 14:58:30 +0530

    05 AI Jobs : Implement the ai jobs with the inngest agentkit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
-import React from "react";
 import { toast } from "sonner";
+import React, { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { useTRPC } from "@/trpc/client";
 
 const Page = () => {
+  const [value, setValue] = useState("");
+
   const trpc = useTRPC();
   const invoke = useMutation(
     trpc.invoke.mutationOptions({
@@ -16,9 +21,10 @@ const Page = () => {
   );
   return (
     <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
       <Button
         disabled={invoke.isPending}
-        onClick={() => invoke.mutate({ text: "john" })}
+        onClick={() => invoke.mutate({ value: value })}
       >
         Invoke Background Jobs
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,28 @@
+import { openai, createAgent } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "writer",
+      system:
+        "You are expert next.js developer. you can write readable, maintainable code. You write a simple Next.js & React snippets.",
+      model: openai({
+        apiKey: process.env.OPENAI_API_KEY,
+        baseUrl: "https://api.chatanywhere.tech/v1",
+        model: "gpt-4.1",
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`
+    );
+
+    console.log(output);
+
+    return { output };
   }
 );
